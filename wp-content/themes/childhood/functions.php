f8bace550f9168b06e6937782de3db832023f87a
Alex Grsm <alexey.grsm@gmail.com>
2023-01-26 12:46:43 +0200

    header+footer

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -8,7 +8,6 @@ function childhood_styles()
     // wp_enqueue_style('header-style', get_template_directory_uri() . '/assets/styles/main.min.css');
     // wp_enqueue_style('animate-style',  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css');
 }
-
 function childhood_scripts()
 {
     wp_enqueue_script('childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
