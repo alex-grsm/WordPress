ab30bebd5aef5a50adbb4390093e6e854bc30864
Alex Grsm <alexey.grsm@gmail.com>
2023-01-29 16:33:39 +0200

    the end

---
+++ wp-content/themes/childhood/404.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+?>
+
+        <div class="container toys">
+            <h2 class="subtitle">
+                <a href="<?= get_home_url(); ?>">Такой страницы не существует. Вернуться на главную?</a>
+            </h2>
+        </div>
+
+<?php
+get_footer();
--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -5,9 +5,9 @@ function childhood_scripts() {
     wp_enqueue_style('childhood-style', get_stylesheet_uri());
 
     wp_enqueue_script('childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js');
-    wp_enqueue_script('jquery');
+    // wp_deregister_script('jquery');
+    // wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js');
+    // wp_enqueue_script('jquery');
 }
 
 
